e712cd30df6836d42a703ce47d10019fd805098c
Mike Starr <imhs@protonmail.com>
2022-02-17 13:35:49 -0800

    mvp for presentation finished

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,14 @@ function Footer() {
   return (
     <div className="footer">
       <p>
-        <strong>github:</strong>
+        <strong>Github:</strong>
         <br />
         <a href="https://github.com/personnamedmike">personnamedmike</a>
         <br />
         <a href="https://github.com/tzjorden">tzjorden</a>
       </p>
       <p>
-        <strong>Data from</strong> <br />
+        <strong>Data pulled from:</strong> <br />
         <a href="https://www.coingecko.com/en/api">CoinGecko</a>
       </p>
     </div>
